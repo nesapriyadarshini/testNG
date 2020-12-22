68caaa7c23a3d8c738402a1f5de3aaa3bc6befac
Nesa <nesapriyadarshini@yahoo.co.in>
2020-12-22 19:50:49 +0530

    Version 2.0 Added Test2.java

---
+++ src/test/java/testng/Test2.java
@@ -0,0 +1,17 @@
+package testng;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test1 {
+  @Test
+  public void f() {
+	  Assert.fail("I am f() and I fail");
+  }
+  
+  
+  @Test(dependsOnMethods={"f"})
+  public void g() {
+	  System.out.println("My dependent f() failed; I'll be skipped");
+  }
+}
